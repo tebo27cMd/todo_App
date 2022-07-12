2395a352f5f9a00e4afb2709c2a821f26d7aea3f
mpho061 <monyamanempho13@gmail.com>
2022-07-12 15:41:12 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 
 import './App.css';
-import Login from './components/login';
-import SignUp from './components/register';
-import React, {useState,useEffect} from 'react'
 
-import {BrowserRouter as Router , Switch, Route} from 'react-router-dom'
+
+import {signInWithGoogle} from "./config/firebase"
+
+
 
 function App() {
-
-  useEffect(()=>{
-   
-    
-  })
-
-
   return (
-    <Router>
-    <Switch>
-      <Route exact path="/"component={Login}></Route>
-      <Route path="/register"component={SignUp}></Route>
-    </Switch>
-  </Router>
+    <div>
+    <button onClick={signInWithGoogle}>Sign in With Google</button>
+  </div>
+
   
   );
 }
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import {getAuth, GoogleAuthProvider,signInWithPopup} from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +18,15 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth=getAuth(app);
+
+const provider = new GoogleAuthProvider()
+ export const signInWithGoogle= () =>{
+  signInWithPopup(auth,provider)
+  .then ((result)=>{
+    console.log(result)
+
+  }).catch((error)=>{
+    console.log(error);
+  });
+};
